8aedf2e7c8ba6c3e192bbaf5014cedf117221a9d
Weixin Zheng <zhengweixin0101@outlook.com>
2025-10-04 10:49:16 +0800

    Update r2_sync.py

--- .github/scripts/r2_sync.py
+++ .github/scripts/r2_sync.py
@@ -2,13 +2,11 @@ import os
 import boto3
 import shutil
 
-# Get env vars
 ACCESS_KEY_ID = os.environ['R2_ACCESS_KEY_ID']
 SECRET_ACCESS_KEY = os.environ['R2_SECRET_ACCESS_KEY']
 ACCOUNT_ID = os.environ['R2_ACCOUNT_ID']
 BUCKET_NAME = os.environ['R2_BUCKET_NAME']
 
-# R2 endpoint
 R2_ENDPOINT = f"https://{ACCOUNT_ID}.r2.cloudflarestorage.com"
 
 session = boto3.session.Session()
@@ -21,24 +19,22 @@ client = session.client(
 )
 
 def sync_bucket():
-    # Delete everything except .git
     for item in os.listdir('.'):
-        if item == '.git':
+        if item in ('.git', '.github'):
             continue
         if os.path.isdir(item):
             shutil.rmtree(item)
         else:
             os.remove(item)
-
-    # List all objects
+            
     paginator = client.get_paginator('list_objects_v2')
     for page in paginator.paginate(Bucket=BUCKET_NAME):
         for obj in page.get('Contents', []):
             key = obj['Key']
-            # Make dirs if needed
+            if key.startswith('.github/'):
+                continue
             if '/' in key:
                 os.makedirs(os.path.dirname(key), exist_ok=True)
-            # Download object
             with open(key, 'wb') as f:
                 client.download_fileobj(BUCKET_NAME, key, f)
 
